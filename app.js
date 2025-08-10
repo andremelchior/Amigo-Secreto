8ae5e6763ceb2da00979d8fe7d8ebe62edd87666
André Melchior <andre.melchior@outlook.com>
2025-08-10 10:06:59 -0300

    feat: add items to the list with friends names

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 let amigos = [];
+let quantidadeAmigos;
 
 function adicionarAmigo(){
     let nome = document.querySelector('#amigo');
     if(nome.value != ""){
         amigos.push(nome.value);
         nome.value = '';
-        for (let i = 0; i < amigos.length; i++) {
-            alert(amigos[i]);   
-        }
+        listarAmigo();
     } else {
         alert("Por favor, insira um nome.");
     }
 
+}
+
+function listarAmigo(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
 }
